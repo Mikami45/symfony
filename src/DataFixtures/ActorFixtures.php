60d051502f1c7019bff0926a35b09fa855f7f8f3
Emilien De Macedo <emilien45100@gmail.com>
2019-12-08 21:28:00 +0100

    Quest 15 Symfony

---
+++ src/DataFixtures/ActorFixtures.php
@@ -0,0 +1,51 @@
+<?php
+
+
+namespace App\DataFixtures;
+
+
+use App\Entity\Actor;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Faker;
+
+class ActorFixtures extends Fixture implements DependentFixtureInterface
+{
+    const ACTORS = [
+        'Andrew Lincoln',
+        'Norman Reedus',
+        'Danai Gurira',
+        'Lauren Cohan',
+    ];
+    public function load(ObjectManager $manager)
+    {
+
+        for ($i = 1; $i <= 50; $i++) {
+            $actor = new Actor();
+            $faker  =  Faker\Factory::create('us_US');
+            $actor->setName($faker->name);
+            $manager->persist($actor);
+            $actor->addProgram($this->getReference('program_' . rand(0 , 5)));
+        }
+
+        foreach (self::ACTORS as $key => $actorName) {
+            $actor = new Actor();
+            $actor->setName($actorName);
+            $actor->AddProgram($this->getReference('program_0'));
+            $manager->persist($actor);
+            $this->addReference('actor_' . $key, $actor);
+        }
+
+        $manager->flush();
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getDependencies()
+    {
+        return [ProgramFixtures::class];
+    }
+
+}
---
+++ src/DataFixtures/EpisodeFixtures.php
@@ -0,0 +1,28 @@
+<?php
+namespace App\DataFixtures;
+use App\Entity\Episode;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Faker;
+class EpisodeFixtures extends Fixture implements DependentFixtureInterface
+{
+    public function load(ObjectManager $manager)
+    {
+        for ($i=0 ; $i<50 ; $i++){
+            $episode=new Episode();
+            $faker = Faker\Factory::create('us_US');
+            $episode->setTitle($faker->sentence);
+            $episode->setNumber($faker->randomDigit);
+            $episode->setSynopsis($faker->paragraph);
+            $episode->setSeason($this->getReference('season_' . rand(0 , 49)));
+            $manager->persist($episode);
+            $this->addReference('episode_' . $i, $episode);
+        }
+        $manager->flush();
+    }
+    public function getDependencies()
+    {
+        return [SeasonFixtures::class];
+    }
+}
